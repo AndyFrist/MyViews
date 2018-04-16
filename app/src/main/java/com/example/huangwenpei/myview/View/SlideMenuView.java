77e279fd96649f09d162b67f380b03804b4e256e
ex-xuxiaopeng001 <1609798079@qq.com>
2018-04-16 23:02:34 +0800

    上传第一次代码

---
+++ app/src/main/java/com/example/huangwenpei/myview/View/SlideMenuView.java
@@ -0,0 +1,24 @@
+package com.example.huangwenpei.myview.View;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by
+ * ex-xuxiaopeng001 on 2018/4/16.
+ */
+
+public class SlideMenuView extends FrameLayout{
+    public SlideMenuView(Context context) {
+        super(context);
+    }
+
+    public SlideMenuView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SlideMenuView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
