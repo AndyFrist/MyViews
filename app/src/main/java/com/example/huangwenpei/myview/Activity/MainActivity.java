938549bbc9bc8bf6d2b01abb39c73e150c8acaa5
firstAndy <a789123456>
2018-04-16 22:44:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/huangwenpei/myview/Activity/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.huangwenpei.myview.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.huangwenpei.myview.R;
+
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+    private Button sildemenu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        sildemenu = findViewById(R.id.sildemenu);
+        sildemenu.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent();
+        switch (view.getId()) {
+            case R.id.sildemenu:
+                intent.setClass(this, SlidemenuActivity.class);
+                break;
+        }
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/huangwenpei/myview/MyApp.java
@@ -0,0 +1,15 @@
+package com.example.huangwenpei.myview;
+
+import android.app.Application;
+
+/**
+ * Created by
+ * huangwenpei on 2018/4/16.
+ */
+
+public class MyApp extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
