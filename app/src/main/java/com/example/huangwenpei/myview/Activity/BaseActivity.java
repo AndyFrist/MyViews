b38705bbfe25109024616ac7953318878f78f80d
ex-xuxiaopeng001 <1609798079@qq.com>
2018-04-18 18:14:40 +0800

    将滑动退出代码整合集中，便于复制

--- app/src/main/java/com/example/huangwenpei/myview/Activity/BaseActivity.java
+++ app/src/main/java/com/example/huangwenpei/myview/Activity/BaseActivity.java
@@ -2,6 +2,7 @@ package com.example.huangwenpei.myview.Activity;
 
 import android.os.Bundle;
 
+import com.example.huangwenpei.myview.Util.StatusBarUtil;
 import com.example.huangwenpei.myview.Util.sliding.SlidingActivity;
 
 
@@ -10,6 +11,7 @@ public class BaseActivity extends SlidingActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StatusBarUtil.translucentStatusBar(this);
     }
 
 
--- app/src/main/java/com/example/huangwenpei/myview/Activity/SlidemenuActivity.java
+++ app/src/main/java/com/example/huangwenpei/myview/Activity/SlidemenuActivity.java
@@ -1,14 +1,34 @@
 package com.example.huangwenpei.myview.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.huangwenpei.myview.R;
+import com.example.huangwenpei.myview.Util.sliding.IntentUtils;
 
-public class SlidemenuActivity extends BaseActivity {
+public class SlidemenuActivity extends BaseActivity implements View.OnClickListener {
+
+    private TextView main_tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slidemenu);
+        main_tv = findViewById(R.id.main_tv);
+        main_tv.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent();
+        switch (view.getId()) {
+            case R.id.main_tv:
+                intent.setClass(this, ChartLineActivity.class);
+                break;
+        }
+        IntentUtils.getInstance().startActivity(this, intent);
     }
 }
