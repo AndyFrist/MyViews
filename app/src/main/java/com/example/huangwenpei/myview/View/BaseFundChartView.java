6c98a7f5730da05b69248a5e832e2a598b4d67b6
ex-xuxiaopeng001 <1609798079@qq.com>
2018-05-14 18:10:43 +0800

    自定义圆形扫二维码完善

--- app/src/main/java/com/example/huangwenpei/myview/View/BaseFundChartView.java
+++ app/src/main/java/com/example/huangwenpei/myview/View/BaseFundChartView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Point;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
@@ -29,10 +30,11 @@ public class BaseFundChartView extends View implements Runnable {
     //一个dp
     private float dimens;
     //直径
-    private float diameter = AndroidUtil.getScreenWidth(MyApp.getContext()) / 2;
+    private float diameter = AndroidUtil.getScreenWidth(MyApp.getContext()) / 3 * 2;
     //半径
     private float radius = diameter / 2;
-
+    //圆心点
+    private Point point = new Point();
     private Paint paint1 = new Paint();
     //4个扇形所在区域
     private RectF re1;
@@ -48,7 +50,10 @@ public class BaseFundChartView extends View implements Runnable {
     public BaseFundChartView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         dimens = context.getResources().getDimension(R.dimen.width_01);
-        re1 = new RectF(0 + radius + 10 * dimens, 0 + radius + 10 * dimens, (diameter + radius - 10 * dimens), (diameter + radius - 10 * dimens));
+        height = 100 * dimens;
+        point.x = AndroidUtil.getScreenWidth(MyApp.getContext()) / 2;
+        point.y = (int) diameter;
+        re1 = new RectF(point.x - radius + 10 * dimens, 0 + radius + 10 * dimens, (diameter + point.x - radius - 10 * dimens), (diameter + radius - 10 * dimens));
     }
 
     private void init() {
@@ -67,7 +72,7 @@ public class BaseFundChartView extends View implements Runnable {
 
         //画透明圆
         paint.setColor(0xff0000ff);
-        canvas.drawCircle(diameter, diameter, radius, paint);
+        canvas.drawCircle(point.x, point.y, radius, paint);
 
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT));
 
@@ -76,10 +81,10 @@ public class BaseFundChartView extends View implements Runnable {
         canvas.drawRect(re, paint);
 
         //画蓝色的圆环
-        paint1.setColor(0xff0000ff);
+        paint1.setColor(0xff1389ff);
         paint1.setStyle(Paint.Style.STROKE);//设置为空心
         paint1.setStrokeWidth(6 * dimens);
-        canvas.drawCircle(diameter, diameter, radius, paint1);
+        canvas.drawCircle(point.x, point.y, radius, paint1);
 
         //画4个扇形
         paint1.reset();
@@ -94,28 +99,29 @@ public class BaseFundChartView extends View implements Runnable {
         //画扫动线
         paint.setStyle(Paint.Style.FILL);
         paint.setAntiAlias(true);
-        Shader mShader = new LinearGradient(0, bar_Y + radius, 0, bar_Y + radius + 100 * dimens, new int[]{0x00ffffff, 0xff0000ff}, new float[]{0.1f, 0.8f}, Shader.TileMode.CLAMP);
+        Shader mShader = new LinearGradient(0, bar_Y + radius, 0, bar_Y + radius + height, new int[]{0x00ffffff, 0xff1389ff}, new float[]{0.1f, 0.8f}, Shader.TileMode.CLAMP);
         paint.setShader(mShader);
         Path path = new Path();
-        path.addCircle(diameter, diameter, radius - 10 * dimens, Path.Direction.CW);
+        path.addCircle(point.x, point.y, radius - 10 * dimens, Path.Direction.CW);
         canvas.clipPath(path);
-        canvas.drawRect(diameter - radius, bar_Y + radius, diameter + radius, bar_Y + radius + 100 * dimens, paint);
+        canvas.drawRect(point.x - radius, bar_Y + radius, point.x + radius, bar_Y + radius + height, paint);
 
     }
 
     private boolean isRun = false;
     private float bar_Y = 0;
+    //渐变高度
+    private float height = 0;
 
     @Override
     public void run() {
         while (isRun) {
             if (bar_Y > diameter) {
-                bar_Y = 0;
+                bar_Y = -height;
             }
             bar_Y = bar_Y + 2 * dimens;
-
             try {
-                Thread.sleep(100);
+                Thread.sleep(10);
             } catch (InterruptedException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
